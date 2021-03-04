94691d576372b03550d46a5e48a0d7606ca67933
jakiichu <72372517+jakiichu@users.noreply.github.com>
2021-03-04 13:27:31 +0700

    04.03.2021

---
+++ 4/js/main.js
@@ -0,0 +1,43 @@
+let app = new Vue({
+  el: '.game',
+  data: {
+      styleObject: {
+        position: "absolute",
+        width: "90px",
+        height: "60px",
+        left:"random()"
+      },
+    rawHtml: '',
+    fish: [],
+
+
+      game: false,
+      left:'50'
+  },
+  
+  methods: {
+    start: function(){
+      this.game = true;
+    },
+    createOneDiv: function(id){
+      this.fish[id] = id;
+    },
+    createMoreBlockDivFish: function(iteration){
+      for(i = 0; iteration > i; i++){
+          this.createOneDiv(i);
+          console.log('рыба');
+      }
+    },
+    test: function(){
+      this.createMoreBlockDivFish(100);
+      console.log(this.fish);
+    },
+    random: function(){
+      return Math.floor(Math.random() * Math.floor(50));
+    }
+  }
+});
+
+
+
+
