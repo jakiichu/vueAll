16c58c7300cab72ab79f8cf57681749897c30788
jakiichu <72372517+jakiichu@users.noreply.github.com>
2021-03-06 10:57:03 +0700

    06.03.2021

--- 4/js/main.js
+++ 4/js/main.js
@@ -1,43 +1,101 @@
-let app = new Vue({
-  el: '.game',
+// let app = new Vue({
+//   el: '.game',
+//   data: {
+
+//     rawHtml: '',
+//     fish: [],
+//     styleObject: {
+//         position: "absolute",
+//         width: "90px",
+//         height: "60px",
+//         left: "10px",
+//         top: "" + "px"
+//       },
+//     game: false,
+//     left:'50px'
+//   },
+
+//   methods: {
+
+//     start: function(){
+//       this.game = true;
+//       this.startGame(this.game);
+//       this.styleObject.top = this.random() + "px"
+//     },
+
+//     startGame: function(game){
+//       this.test()
+//     },
+
+//     createOneDiv: function(id){
+//       this.fish[id] = id;
+//       this.fish.splice(id, 0,id)
+//     },
+
+//     createMoreBlockDivFish: function(iteration){
+//       for(i = 0; iteration > i; i++){
+//           this.createOneDiv(i)
+//           console.log('рыба')
+
+//       }
+//     },
+
+//     test: function(){
+//       this.createMoreBlockDivFish(100);
+//       console.log(this.fish);
+//     },
+
+//     random: function(){
+//       return Math.floor(Math.random() * Math.floor(50));
+//     }
+
+//   }
+// });
+
+let app1 = new Vue({
+  el: ".game1",
   data: {
-      styleObject: {
+    rawHtml: "",
+    fish: [],
+    styleObject: {
+      position: "absolute",
+      width: "90px",
+      height: "60px",
+      left: "10px",
+      top: "" + "px",
+    },
+    game: false,
+  },
+
+  methods: {
+    createMoreBlockDivFish: function (iteration) {
+      for (i = 0; iteration > i; i++) {
+        this.createOneDiv(i);
+        console.log("рыба");
+      }
+    },
+
+    createOneDiv: function (id) {
+      this.fish[id] = id;
+      let asd = {
         position: "absolute",
         width: "90px",
         height: "60px",
-        left:this.left
-      },
-    rawHtml: '',
-    fish: ['',''],
-
-
-      game: false,
-      left:'50px'
-  },
-  
-  methods: {
-    start: function(){
-      this.game = true;
-    },
-    createOneDiv: function(id){
-      this.fish[id] = id;
-    },
-    createMoreBlockDivFish: function(iteration){
-      for(i = 0; iteration > i; i++){
-          this.createOneDiv(i);
-          console.log('рыба');
-      }
-    },
-    test: function(){
-      this.createMoreBlockDivFish(100);
+        left: "10px",
+        top: "" + "px",
+      };
+      asd.left = this.randomLeft() + "px";
+      asd.top = this.randomTop() + "px";
+      console.log(asd.left);
+      console.log(asd.top);
       console.log(this.fish);
+      this.fish.splice(id, 1, asd);
     },
-    random: function(){
-      return Math.floor(Math.random() * Math.floor(50));
-    }
-  }
+    randomLeft: function () {
+      return Math.floor(Math.random() * Math.floor(1820));
+    },
+    randomTop: function () {
+      return Math.floor(Math.random() * Math.floor(860));
+    },
+  },
 });
-
-
-
-
